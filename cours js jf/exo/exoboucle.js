4bde6e353677dd8900dc6c79dc62c2eaf33e8a89
Busy223 <las.sissoko27@gmail.com>
2025-01-07 11:03:58 +0100

    ajout const

--- cours js jf/exo/exoboucle.js
+++ cours js jf/exo/exoboucle.js
@@ -2,6 +2,7 @@ const allP = document.body.getElementsByTagName("p");
 console.log(allP);
 
 const pTableau = Array.from(allP);
+console.log(pTableau);
 
 pTableau.map((paragraphe) => {
     paragraphe.innerText = "LOL JE SUIS HACKERMAN";
