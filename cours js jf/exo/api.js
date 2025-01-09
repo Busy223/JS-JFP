9018a6c70afd875fe33691a7dcc9d5ebef0873d8
Busy223 <las.sissoko27@gmail.com>
2025-01-09 15:19:32 +0100

    l
    l

--- cours js jf/exo/api.js
+++ cours js jf/exo/api.js
@@ -128,7 +128,7 @@ const contactApiPoke2 =  async () => {
         console.log(pokedata2);
 
         if (!pokedata2.ok|| pokedata2.status !==200){
-            console.error("erreur lors de la recup de données: " , pokedata.statusText);
+            console.error("erreur lors de la recup de données: " , pokedata2.statusText);
             return;
         }
         const convertJsonEnJsPokeApi2 = await pokedata2.json();
@@ -156,3 +156,38 @@ const contactApiPoke2 =  async () => {
 
 contactApiPoke2();
 
+//!correction 
+let divPokemonUI = document.querySelector("#superPokemonList");
+
+const fetchPokemonGen9 = async () => {
+    try {
+        const response = await fetch("https://tyradex.vercel.app/api/v1/gen/9");
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des données');
+        }
+        const data = await response.json();
+        console.log(data);
+        
+
+        // Générer des cartes pour chaque Pokémon
+        data.forEach(pokemon => {
+            const cartePokemon = document.createElement('div');
+            cartePokemon.classList.add('card','m-3', 'p-3','bg-light');
+            cartePokemon.style.width = '18rem';
+            cartePokemon.innerHTML = DOMPurify.sanitize(`
+              <img src="${pokemon.sprites.regular}" class="card-img-top" alt="${pokemon.name.fr}">
+              <div class="card-body">
+                <h5 class="card-title">${pokemon.name.fr}</h5>
+                <p class="card-text">Type: ${pokemon.types.map(type => type.name).join(', ')}</p>
+                <p class="card-text">Poids: ${pokemon.weight}</p>
+                <p class="card-text">Taille: ${pokemon.height}</p>
+            </div>`);
+            divPokemonUI.appendChild(cartePokemon);
+            // Ajouter la carte au conteneur
+        });
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
+
+fetchPokemonGen9();
