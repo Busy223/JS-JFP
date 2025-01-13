cf06358404773618dbcb041ce028030cdf42c49d
Busy223 <las.sissoko27@gmail.com>
2025-01-13 14:34:20 +0100

    .
    .

---
+++ cours js jf/exo/Evalapi.js
@@ -0,0 +1,42 @@
+const card = document.querySelector('.card');
+
+const contactApi = async () => {
+    try {
+        const recupApi = await fetch('https://randomuser.me/api/');
+        console.log(recupApi);
+        if (!recupApi.ok || recupApi.status !== 200) {
+            console.error("Erreur lors de la récupération des données : ", recupApi.statusText);
+            return;
+        }
+
+        const transformedData = await recupApi.json();
+        console.log(transformedData);
+
+        const user = transformedData.results[0];
+        if (!user) {
+            console.error("Aucun utilisateur trouvé");
+            return;
+        }
+
+
+        const cardrandom = document.createElement('div');
+        cardrandom.classList.add('card', 'm-3', 'p-3', 'bg-light');
+        cardrandom.innerHTML = DOMPurify.sanitize(`
+            <img src="${user.picture.medium}" class="card-img-top" alt="random image">
+            <div class="card-body">
+                <h5 class="card-title">${user.gender}</h5>
+                <p class="card-nom"> ${user.name.first}</p>
+                <p class="card-prenom"> ${user.name.last}</p>
+                <p class="card-adress"> ${user.location.street.number} ${user.location.street.name}</p>
+                <p class="card-tel"> ${user.phone}</p>
+                <button id="myButton" class="btn btn-primary" onclick="history.go(0)">go somewhere</button></div>`);
+        card.appendChild(cardrandom);
+
+    } catch (error) {
+        console.error("Erreur lors de l'appel à l'API : ", error);
+    }
+};
+
+contactApi();
+
+
--- cours js jf/exo/classepme.js
+++ cours js jf/exo/classepme.js
@@ -69,3 +69,39 @@ const pme = new Pme(
 
 pme.bilanCalculed();
 console.log(pme.bilanCalculed());
+
+
+//! correction 
+class Pme {
+    // 
+    constructor(nom, equipe, ventes, coutsFixes, achats) {
+        this._nom = nom;
+        this._equipe = equipe;
+        this._cout = coutsFixes + achats;// On peut assigner directement un calcul ici
+        this._ventes = ventes;
+        this._bilan = 0;    // attribut en OutMode a calculer
+    }
+
+    bilanCalculed() {
+        console.log(this._equipe);
+        let cumulEquipe = 0;
+        console.log(`${this._nom} : Cout Initial : ${this._cout}`);
+
+        //Boucle qui parcourt le tableau des salariés, Employee (equipe)
+        //Sur chaque salarié parcouru dans le tableau, on récupère et cumule le cout de ce Salarié
+        for (let unSalarie of this._equipe) {
+            cumulEquipe += unSalarie.getCout();
+        }
+
+        console.log(`${this._nom} : Cout Total Equipe : ${cumulEquipe}`);
+        //Ensuite dans les couts de l'entreprise on cumul le cout de toute l'équipe
+        this._cout += cumulEquipe;
+        console.log(`${this._nom} : VENTES : ${this._ventes}`);
+        //Dans les _cout on va avoir les frais fixe + frais achat et 
+        //on vient de rajouter en + le cout total d'une equipe
+        //donc le bilan de la pme : les ventes moins tous les couts (frais fixes, achat + cout total de l'equipe à l'année)
+        this._bilan = this._ventes - this._cout;
+        console.log(`${this._nom} : BILAN : ${this._bilan}`);
+    }
+}
+
