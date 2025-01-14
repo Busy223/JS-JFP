1f87bafbbca72988bae3c30ac61a6a30c2f2e66b
Busy223 <las.sissoko27@gmail.com>
2025-01-14 17:02:08 +0100

    .
    .

--- firebase/firebasetest.js
+++ firebase/firebasetest.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBdyEFlFh4XRXS6lZ0pxnMzyiTISbiFlLE",
     authDomain: "bugsbusy-e46c4.firebaseapp.com",
-    databaseURL: "https://bugsbusy-e46c4-default-rtdb.europe-west1.firebasedatabase.app",
+    databaseURL: "https://bugsbusy-e46c4-default-rtdb.europe-west1.firebasedatabase.app/",
     projectId: "bugsbusy-e46c4",
     storageBucket: "bugsbusy-e46c4.firebasestorage.app",
     messagingSenderId: "199865224805",
@@ -20,10 +20,45 @@ firebase.initializeApp(firebaseConfig);
  * Référence à la base de données Firebase.
  * @type {Object}
  */
-const dbRef = firebase.database().ref();
+const dbRef = firebase.database().ref();// crée des référence chez firebase dbRef est la ref principale de la base de donée 
 
 /**
  * Référence au noeud "tasks" dans la base de données.
  * @type {Object}
  */
 const allTasksRef = dbRef.child("tasks");
+
+/**
+ * Référence au bouton d'ajout de tâche dans l'interface utilisateur.
+ * @type {HTMLElement}
+ */
+const addBtnUI = document.querySelector('#addTaskButton');
+
+/**
+ * Référence à la liste des tâches dans l'interface utilisateur.
+ * @type {HTMLElement}
+ */
+const tasksListUI = document.querySelector('#taskList');
+
+/**
+ * Ajoute un écouteur d'événements au bouton d'ajout de tâche.
+ * @event click
+ * @param {Function} addTask - Fonction appelée lors du clic sur le bouton.
+ */
+addBtnUI.addEventListener('click', addTask);
+
+/**
+ * Ajoute une nouvelle tâche à la base de données.
+ * @function addTask
+ * @description Cette fonction récupère la valeur de l'input de tâche, 
+ * crée un nouvel objet tâche avec cette valeur, 
+ * l'ajoute à la base de données Firebase, 
+ * vide l'input et met à jour l'affichage des tâches.
+ */
+function addTask() {
+  const taskInputUI = document.querySelector('#taskInput');
+  let newTask = {};
+  newTask.title = taskInputUI.value;
+  allTasksRef.push(newTask);
+  taskInputUI.value = "";
+}
